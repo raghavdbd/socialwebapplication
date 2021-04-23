f9969e0199b6412dbf33b1970562432d00c7d242
raghav goyal <raghavgoyaldbd4782gmail.com>
2021-04-23 20:42:34 +0530

    add delete button for posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,5 +1,6 @@
 const Posts=require('../models/posts')
 // craeting controller for posts
+const Commnets=require('../models/comments')
 module.exports.create=function(req,res){
     // we create a post
     Posts.create({
@@ -17,4 +18,19 @@ module.exports.create=function(req,res){
     
     )
 
+};
+module.exports.destroy=function(req,res){
+    Posts.findById(req.params.id,function(err,post){
+        // .id converting object id into String
+        if(post.user==req.user.id){
+            // remove post
+            post.remove();
+            // delete all comments with associated post
+            Commnets.deleteMany({post:req.params .id},function(err){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
 }
