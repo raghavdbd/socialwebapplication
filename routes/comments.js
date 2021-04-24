3f6297437fdc89d4f35e03aadde9c2073a7f3228
raghav goyal <raghavgoyaldbd4782gmail.com>
2021-04-24 16:02:30 +0530

    add delete button for commnets

--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,10 @@
 const express=require('express');
-const passport=require('passport');
 const router = express.Router();
+const passport=require('passport');
+
 
 const CommentController=require('../controllers/comments_controller')
 
 router.post('/create',passport.checkAuthentication,CommentController.create);
+router.get('/destroy/:id',passport.checkAuthentication,CommentController.destroy);
 module.exports=router;
--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,5 @@ router.get('/',homeController.home);
 router.use('/users',require('./users'));
 router.use('/posts',require('./posts'));
 router.use('/comments',require('./comments'))
-console.log('router loaded');
+// console.log('router loaded');
 module.exports=router;
